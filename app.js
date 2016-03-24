fde790f60c7fde1dd3094e73c931b0b0985eb869
zhanggj <zhanggjwei@163.com>
2016-03-24 17:44:40 +0800

    增加了session功能。

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var settings = require('./settings');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
+var fs = require('fs');
+var accessLog = fs.createWriteStream('access.log',{flags:'a'});
 
 var app = express();
 
@@ -17,13 +22,24 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(logger({stream:accessLog}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret : settings.cookieSecret,
+  key : settings.db,
+  cookie : {maxAge : 1000*60*60*24},
+  store : new MongoStore({
+    url: settings.url,
+  }),
+  resave: settings.resave,
+  saveUninitialized : settings.saveUninitialized
+}));
+
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  cookieSecret: 'node-angular-FriendLine',
+  db:'friendLine',
+  host:'localhost',
+  port: 27017,
+  resave : false,
+  saveUninitialized : false,
+  url : 'mongodb://localhost/friendLine'
+};
