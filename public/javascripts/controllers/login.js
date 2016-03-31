1fa9cf5d23b5139022c22d0c9a9aa647823c6097
zhanggj <zhanggjwei@163.com>
2016-03-31 23:09:09 +0800

    添加了登录页面。

--- public/javascripts/controllers/login.js
+++ public/javascripts/controllers/login.js
@@ -1,10 +1,4 @@
 angular.module('myApp.controller',[])
   .controller('loginCtrl',function($http){
-    $http({method: 'GET', url: '/test'}).
-   success(function(data, status, headers, config) {
-    console.log(data);
-   }).
-   error(function(data, status, headers, config) {
-    console.log('发生所悟');
-   });
+    
   });
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/test', function(req, res, next) {
   res.json({msg:'hahaha'});
 });
