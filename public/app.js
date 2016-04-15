651718f6c1a951375523f3c64c79c8c775a23cac
zhanggj <zhanggjwei@163.com>
2016-04-15 20:32:08 +0800

    更换了ide，代码格式上变了。

--- public/app.js
+++ public/app.js
@@ -7,44 +7,44 @@ angular
     'ui.router',
     'ngAnimate'
   ])
-  .run(function($state){
+  .run(function($state) {
     console.log('.run方法执行了');
   })
-  .config(function($stateProvider,$urlRouterProvider,$httpProvider){
+  .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
     $stateProvider
-      .state('login',{
-        url:'/login',
+      .state('login', {
+        url: '/login',
         abstract: true,
         templateUrl: '/pages/login.html',
         controller: 'loginCtrl'
       })
-      .state('login.lo',{
-        url:'/loginlo',
+      .state('login.lo', {
+        url: '/loginlo',
         templateUrl: '/pages/login-lo.html',
         controller: 'loginloCtrl'
       })
-      .state('login.register',{
-        url:'/register',
+      .state('login.register', {
+        url: '/register',
         templateUrl: '/pages/login-register.html',
         controller: 'registerCtrl'
       })
-      .state('main',{
-        url:'/main',
-        templateUrl:'/pages/main.html',
-        controller:'mainCtrl'
+      .state('main', {
+        url: '/main',
+        templateUrl: '/pages/main.html',
+        controller: 'mainCtrl'
       });
 
-      $urlRouterProvider.otherwise('/login/loginlo');
-      $httpProvider.defaults.transformResponse.push(function(response){
-        if(response.hasOwnProperty('errcode')){
-          if(response.errcode !==0){
-            throw response;
-          }else{
-            return response.data;
-          }
-        }else{
-          return response;
+    $urlRouterProvider.otherwise('/login/loginlo');
+    $httpProvider.defaults.transformResponse.push(function(response) {
+      if (response.hasOwnProperty('errcode')) {
+        if (response.errcode !== 0) {
+          throw response;
+        } else {
+          return response.data;
         }
-      });
-      $httpProvider.interceptors.push('myHttpIntercepter');
+      } else {
+        return response;
+      }
+    });
+    $httpProvider.interceptors.push('myHttpIntercepter');
   });
--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,23 @@ var router = express.Router();
 var register = require('../controllers/register');
 var login = require('../controllers/login');
 
-router.get('/',function(req,res,next){
-  res.sendFile(path.join(__dirname,'/public/index.html'));
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 router.post('/register', function(req, res, next) {
-  register.register(req,res,next);
+  register.register(req, res, next);
 });
 
-router.post('/checkByLoginname',function(req,res,next){
-  register.getUserByName(req,res,next);
+router.post('/checkByLoginname', function(req, res, next) {
+  register.getUserByName(req, res, next);
 });
 
-router.post('/login',function(req,res,next){
-  login.login(req,res,next);
+router.post('/login', function(req, res, next) {
+  login.login(req, res, next);
 });
 //没有匹配的路由，有err参数应该不会走这个中间件了
-router.use(function(req,res,next){
+router.use(function(req, res, next) {
   console.log(req.url);
   console.log('没有找到路由');
   next();
